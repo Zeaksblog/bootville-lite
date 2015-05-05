9d3a4af1a4f7dd5a101cfb935b3f107c6ea1c6a1
Scott <zeaks@live.ca>
2015-05-05 19:24:56 -0300

    Updated discount code

--- inc/admin.php
+++ inc/admin.php
@@ -40,7 +40,7 @@ function bootville_lite_admin() { ?>
 			?>
 		</p>
 		<p><a class="cta-button" href="http://www.themes.zeaks.org/themes/bootville" target="_blank"><?php echo sprintf(__( 'View the Full Version of %1$s', 'bootville_lite' ), BOOTVILLE_FULL_NAME ); ?></a></p>
-		<p><?php echo BOOTVILLE_LITE_NAME . __( ' users get 25% off by using Coupon Code: <strong>LITE25</strong> at checkout.', 'bootville_lite' ); ?></p>
+		<p><?php echo BOOTVILLE_LITE_NAME . __( ' users get 20% off by using Coupon Code: <strong>LITE20</strong> at checkout.', 'bootville_lite' ); ?></p>
 		<div class="screenshot">
 			<img class="bootville-screenshot" src="<?php echo BOOTVILLE_IMG . 'bootville-full.png'; ?>" alt="Bootville">
 		</div>
